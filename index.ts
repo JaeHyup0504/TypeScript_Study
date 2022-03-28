633033a110daf0159878a2cff53481abd2555e2e
이재협 <zzok3312@naver.com>
2022-03-29 00:23:22 +0900

    Add: 필수 문법

--- index.ts
+++ index.ts
@@ -11,3 +11,21 @@ let 이름4 :myType = 123;
 function 함수(x: number) : number {
     return x * 2
 }
+
+// tuple 타입
+type Member = [number, boolean];
+let john:Member = [123, true]
+
+type Member2 = {
+    // 글자로 된 모든 object 속성의 타입은: string
+    [key: string]: string
+}
+let jon : Member2 = {name:'kim', age: '123'}
+
+// class 타입지정 가능
+class User {
+    name;
+    constructor(name){
+        this.name= name;
+    }
+}
