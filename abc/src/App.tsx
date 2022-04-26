bfa85939c9c3636dbdc3b6cfd4ad4f2704fc5394
이재협 <zzok3312@naver.com>
2022-04-26 23:42:20 +0900

    Add: 추가

--- abc/src/App.tsx
+++ abc/src/App.tsx
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import {RootState, increment} from './index'
+
 import './App.css';
 
 let a :string = 'Lee'
@@ -15,10 +18,15 @@ function App() {
   // string 또는 number가 들어올 state를 만들고 싶을 때
   let [user1, setUser1] = useState<string | number>('Lee')
 
+  const type = useSelector((state :RootState) => state)
+  const dispatch = useDispatch()
+
   return (
     <div>
      { 박스1 }
      <h4>안녕하십니까</h4>
+     {type.counter1.count}
+     <button onClick={()=>{dispatch(increment())}}>버튼</button>
      <Profile name="철수" age="20"></Profile>
     </div>
   );
--- abc/src/index.tsx
+++ abc/src/index.tsx
@@ -3,17 +3,47 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createSlice, configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const 초기값 = { count: 0, user : 'kim' };
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState : 초기값,
+  reducers: {
+    increment (state){
+      state.count += 1
+    },
+    decrement (state){
+      state.count -= 1
+    },
+    incrementByAmount (state, action :any){
+      state.count += action.payload
+    }
+  }
+})
+
+let store = configureStore({
+  reducer: {
+    counter1 : counterSlice.reducer
+  }
+})
+
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
-// export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>
+export let {increment, decrement, incrementByAmount} = counterSlice.actions
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
