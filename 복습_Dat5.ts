a588ec96ec78d0981ba9b487afd36eb04bb0ec8d
이재협 <zzok3312@naver.com>
2022-05-21 23:38:14 +0900

    Add: 복습_Day5

---
+++ 복습_Dat5.ts
@@ -0,0 +1,10 @@
+function 복습5(x :number) : number{
+    return x * 2
+}
+
+복습5(20)
+
+function 복습5_1(x :number | string) : void {
+    // 실수로 뭔가 return 하는것을 사전에 막는다.
+    // 변수? :number는 number | nudefined와 같음 
+}
